40178bffbbb6d1dd7f1ecc14101f1752b41b8e75
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2021-02-04 10:47:26 -0300

    Database Seed, declared Books route and Controller for all books, tests

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -7,12 +7,6 @@ class CoverUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  def default_url
-    url = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/default/cover.jpg"
-    url.prepend('/') unless url[0] == '/'
-    url
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -20,12 +14,11 @@ class CoverUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    url = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/default/cover.jpg"
+    url.prepend('/') unless url[0] == '/'
+    url
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
