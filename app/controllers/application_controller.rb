afdd3d76730a0249621470289d53e1a7afb55225
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2021-02-07 15:52:10 -0300

    Author controller and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     rescue_from QueryBuilderError, with: :builder_error
     rescue_from RepresentationBuilderError, with: :builder_error
+    rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
   
     protected
   
@@ -13,6 +14,15 @@ class ApplicationController < ActionController::API
         }
       }
     end
+
+    def unprocessable_entity!(resource)
+        render status: :unprocessable_entity, json: {
+            error: {
+                message: "Invalid parameters for resource #{resource.class}.",
+                invalid_params: resource.errors
+            }
+        }
+    end
   
     def orchestrate_query(scope, actions = :all)
       QueryOrchestrator.new(scope: scope,
@@ -21,4 +31,17 @@ class ApplicationController < ActionController::API
                             response: response,
                             actions: actions).run
     end
+
+    def serialize(data)
+        {
+            json: Alexandria::Serializer.new(   data: data,
+                                                params: params,
+                                                actions: [:fields, :embeds]).to_json
+        }
+    end
+
+    def resource_not_found
+        render(status: 404)
+    end
+
 end
